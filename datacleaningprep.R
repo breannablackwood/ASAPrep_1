845d799f621314773140838c8c4c72597ab42d11
breannablackwood <blackwoo.breanna@ufl.edu>
2026-03-22 20:49:32 -0400

    Update datacleaningprep.R
    Cmpleted column names and subsetting columns into new dataset.

--- datacleaningprep.R
+++ datacleaningprep.R
@@ -8,13 +8,30 @@ library(dplyr)
 #Read in data files 
 #I would recommend downloading it to your computer and reading it in. 
 #This is easiest for me personally. 
-
 data <- read.csv("~/Downloads/Guetschow-et-al-2022-PRIMAP-hist_v2.4_11-Oct-2022.csv")
 head(data) #allows us to see the beginning rows of the data. 
 
+####Changing column names
+#Notice that right now the column names sort of vary from the sheet.
+#What if we want to make it the same? 
+#We can do this using colnames
+colnames(data) <- c("source", "scenario (PRIMAP-hist)", "area (ISO3)", "entity", 
+                    "unit", "category (IPCC2006_PRIMAP)", 1750:2021)
+
+ 
+####Subsetting columns 
 #Now, let's say we want to only focus on certain columns of the data. 
-#Then, we would want to create a new dataset that only has these columns. 
-##Subsetting
+#Then, we would want to create a new dataset that only has these columns.
+#Now let's say we want to subset from 2000 to 2021. 
+#We can do this by using the subset() command
+data21century <- subset(data, select = c("source", "scenario (PRIMAP-hist)", "area (ISO3)", "entity", 
+                                         "unit", "category (IPCC2006_PRIMAP)", 2000:2021))
+head(data21century)
+
+####Subsetting rows
+#But what if we want to do rows? 
+#We can use slice() from the dplyr package, or base r functions 
+#If we want to subset by condition, we can use filter() from dplyr or base r functions 
 
 
 ##Getting rid of NA values 
@@ -25,5 +42,14 @@ head(data) #allows us to see the beginning rows of the data.
 
 ##Creating new variables/columns
 
+#Bind columns and rows 
+
+
 
 #Write out to a CSV. 
+
+
+#Final notes
+#You can use base R commands to do some of these things, which is what I usually do. 
+#But, using package commands may be helpful as well. 
+#Ii would recommend also: tibble, tidyverse, and tinytex packages. 
