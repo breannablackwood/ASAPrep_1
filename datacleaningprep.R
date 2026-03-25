2914e75e79ec8ad5e82bb284c0a19911b8217c20
SienickiCone <sienickicone@gmail.com>
2026-03-25 00:23:54 -0400

    Properly pushing the code this time

--- datacleaningprep.R
+++ datacleaningprep.R
@@ -9,8 +9,8 @@ library(dplyr)
 ####Read in data files 
 #I would recommend downloading it to your computer and reading it in. 
 #This is easiest for me personally. 
-data <- read.csv("~/Downloads/Guetschow-et-al-2022-PRIMAP-hist_v2.4_11-Oct-2022.csv")
-head(data) #allows us to see the beginning rows of the data. 
+data <- read.csv("Guetschow-et-al-2022-PRIMAP-hist_v2.4_11-Oct-2022.csv")
+head(data, 1) #allows us to see the beginning n rows of the data. 
 
 ####Changing column names
 #Notice that right now the column names sort of vary from the sheet.
@@ -29,11 +29,17 @@ data21century <- subset(data, select = c("source", "scenario (PRIMAP-hist)", "ar
                                          "unit", "category (IPCC2006_PRIMAP)", 2000:2021))
 head(data21century)
 
+#Similarly, if you want to exclude some years like 2020, you can do this
+data21centurywo20 <- subset(data, select = c("source", "scenario (PRIMAP-hist)", "area (ISO3)", "entity", 
+                                             "unit", "category (IPCC2006_PRIMAP)", 2000:2019,2021))
+
 ####Subsetting rows
 #But what if we want to do rows? 
-#We can use slice() from the dplyr package, or base r functions 
 #Let's say we want to only have the first 100 rows. 
-data21first100 <- slice(data21century, (1:100))
+data21first100 <- head(data21century, 100)
+#But what if we want to get certain rows like row 50 to row 100?
+#We can use slice() from the dplyr package, or base r functions 
+data21row50to100 <- slice(data21century, (50:100))
 #We can also specify with different slice function minimum values, maximum, etc. 
 #Check the help pane for that information.
 
@@ -49,11 +55,32 @@ data21century <- na.omit(data21century) #Note this will remove any row that cont
 #So we can see that removes all the rows and there is nothing left. 
 #If we want to remove based on certain columns, we can use filter() instead or subset() 
 #For example let's remove the rows with NA in 2000 or 2001 columns
-clean2 <- data21century |>
-  filter(!is.na(`2000`) & !is.na(`2001`))
+clean2 <- filter(data21century,!is.na(`2000`) | !is.na(`2001`))
+#Just make sure you use `` to specifies column name
+
+####Piping
+#Now if we want to get rows 100 to 1000, filter so where you can only have CO2, and filter out NA in 2000 or 2001
+#We can utilize piping where this will be a lot more readable. There are 2 methods, %>% or |>
+cleanfirstmethod <- data21century %>% slice((100:1000)) %>% filter(entity == "CO2") %>% filter(!is.na(`2000`) & !is.na(`2001`))
+#Or we can also do this
+cleansecondmethod <- data21century |> slice((100:1000)) |> filter(entity == "CO2") |> filter(!is.na(`2000`) & !is.na(`2001`))
+#Piping essentially take an object and "pipe" it into the next function without saving the data set in each set to save memory
+#Creating too much data sets in one environment can slow down the computer processing speed especially if it's multiple big data sets
+
 
 ####Transpose rows 
-
+#Now let us find the mean of each unique area first from our data set in the year 2000 to 2021
+MeanByArea <- clean2 %>% group_by(`area (ISO3)`) %>% summarise(across(any_of(as.character(2000:2021)), \(x) mean(x, na.rm = TRUE)))
+#Since your area is unique, but your year is sequential, we should transpose this so that the unique area is your column header
+transposed_matrix <- t(MeanByArea)
+#Turn this into a data.frame
+transposed_area <- as.data.frame(transposed_matrix)
+#Now there's a convoluted way to make your first row as your header, but we will be employing janitor to help us out
+library(janitor)
+clean_transposed_area <- transposed_area %>% row_to_names(row_number = 1)
+#And as usual, you can pipe all of the transposed process in one line like the following:
+clean_transposed_area <- MeanByArea %>% t() %>% as.data.frame() %>% row_to_names(row_number = 1)
+#Please note to still run the library or else the piping will not work
 
 ####Creating new variables/columns
 #To create a new variable, we use the $ operator after the dataset.
@@ -63,8 +90,29 @@ head(data21century)
 #As we can see, the whole column is full of meows. 
 
 ####Bind columns and rows 
-#This is helpful when combining rows and columns from different datasets. 
-#We can do this by using the bind_rows() and bind_cols()
+#Now let us have the following temporary data set
+city_data <- data.frame(
+  City = c("Tokyo", "New York", "London"),
+  Population = c(37400000, 8400000, 8900000),
+  Area_sqkm = c(2194, 783, 1572),
+  GDP_Billion = c(1600, 1700, 978)
+)
+
+# 2. The single-row data frame (1 city, same 3 stats)
+new_city <- data.frame(
+  City = "Paris",
+  Population = 2161000,
+  Area_sqkm = 105,
+  GDP_Billion = 850
+)
+#Now let's say we want to combine the row from new_city to existing city data, how would we go with this?
+#We can do this by using the bind_rows()
+city_data <- bind_rows(city_data,new_city)
+#You can further sort the newly added row by alphabetical order by doing the following:
+city_data <- city_data %>% arrange(City)
+#If you need to sort the city first in descending order then population size, we can do this:
+city_data <- city_data %>% arrange(desc(City), Population)
+#Similarly, you can do the same thing with columns by doing bind_cols(), but make sure that your column name is case sensitive
 #See help documentation for more information.
 
 ####Write out cleaned data to a csv. 
